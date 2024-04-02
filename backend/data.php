ef286c50ba33f4e129506a2e86cd6815b66665c7
aliabbas-2012 <itsgeniusstar@gmail.com>
2024-04-02 22:29:03 +0500

    Minor change

--- backend/data.php
+++ backend/data.php
@@ -10,7 +10,7 @@ $search_result = [
 ];
 for ($i = 0; $i < 100; $i++) {
     $record = [
-        'id' => $i,
+        'id' => $i+1,
         'jpa_number' => 'E6073080' . $i,
         'scanned_jpa' => 'https://scjpc.nyc3.digitaloceanspaces.com/2009%20JPA/FEBRUARY%202009%20JPA/E6073-0800.pdf',
         'received_date' => date('Y-m-d', strtotime("-$i days")),
