11207617057d7c2ff9cf5dd73785d7b840f2d5a2
Ali Abbas <ali@accuratewebsolution.com>
2024-09-16 14:23:30 +0500

    created template file to render contacts table

--- admin/pole_contacts.php
+++ admin/pole_contacts.php
@@ -3,23 +3,7 @@ add_action("elementor/frontend/after_enqueue_styles", "scjpc_enqueue_pole_member
 function scjpc_enqueue_pole_members_script(): void {
   wp_enqueue_style('poles-contacts', SCJPC_ASSETS_URL . 'css/pole-contacts.css', false, '1.26');
   wp_enqueue_style('print', SCJPC_ASSETS_URL . 'css/print.css', array(), '7.9', 'print');
-
 }
-
-add_shortcode('scjpc_pole_inspection_contacts', 'scjpc_pole_inspection_contacts_callback');
-function scjpc_pole_inspection_contacts_callback(): void {
-  ob_start();
-  require_once SCJPC_PLUGIN_FRONTEND_BASE . 'table/pole-contacts.php';
-  echo ob_get_clean();
-}
-
-add_shortcode('scjpc_jpa_contacts', 'scjpc_jpa_contacts_callback');
-function scjpc_jpa_contacts_callback(): void {
-  ob_start();
-  require_once SCJPC_PLUGIN_FRONTEND_BASE . 'table/jpa-contacts.php';
-  echo ob_get_clean();
-}
-
 
 function scjpc_get_pole_inspection_contacts($query = []): array {
   $fields_group = acf_get_field_group("group_662e732c50241"); // Server
@@ -48,14 +32,6 @@ function ajax_scjpc_export_pole_members() {
   $export_file_path = scjpc_process_contacts_csv_writing($response, $field_labels, 'pole');
   wp_send_json_success(['export_file_path' => $export_file_path], 200);
   wp_die();
-}
-
-
-add_shortcode('scjpc_buddy_pole_contacts', 'scjpc_buddy_pole_contacts_callback');
-function scjpc_buddy_pole_contacts_callback(): void {
-  ob_start();
-  require_once SCJPC_PLUGIN_FRONTEND_BASE . 'table/buddy-pole-contacts.php';
-  echo ob_get_clean();
 }
 
 
@@ -88,13 +64,6 @@ function ajax_scjpc_export_buddy_pole_members() {
   wp_die();
 }
 
-add_shortcode('scjpc_graffiti_removal_contacts', 'scjpc_graffiti_removal_contacts_callback');
-function scjpc_graffiti_removal_contacts_callback(): void {
-  ob_start();
-  require_once SCJPC_PLUGIN_FRONTEND_BASE . 'table/graffiti-removal-contacts.php';
-  echo ob_get_clean();
-}
-
 
 function scjpc_get_graffiti_removal_contacts($query = []): array {
 //  $fields_group = acf_get_field_group("group_662e732c50241"); // Server  
@@ -125,14 +94,6 @@ function ajax_scjpc_export_graffiti_removal_members() {
 }
 
 
-add_shortcode('scjpc_emergency_claim_contacts', 'scjpc_emergency_claim_contacts_callback');
-function scjpc_emergency_claim_contacts_callback(): void {
-  ob_start();
-  require_once SCJPC_PLUGIN_FRONTEND_BASE . 'table/emergency-contacts.php';
-  echo ob_get_clean();
-}
-
-
 function scjpc_get_emergency_claim_contacts($query = []): array {
   $fields_group = acf_get_field_group("group_66464f0e51512"); // Server
   $fields = acf_get_fields($fields_group);
@@ -158,13 +119,6 @@ function ajax_scjpc_export_emergency_contacts() {
   wp_die();
 }
 
-// New table for field assistance/joint meet contacts
-add_shortcode('scjpc_field_assistance_contacts', 'scjpc_field_assistance_contacts_callback');
-function scjpc_field_assistance_contacts_callback(): void {
-  ob_start();
-  require_once SCJPC_PLUGIN_FRONTEND_BASE . 'table/field-assistance-contacts.php';
-  echo ob_get_clean();
-}
 
 function scjpc_get_field_assistance_contacts($query = []): array {
   $fields_group = acf_get_field_group("group_664639ed6409d"); // Server
--- frontend/table/contacts.php
+++ frontend/table/contacts.php
@@ -1,11 +1,9 @@
-<?php [$fields, $jpa_contacts] = scjpc_get_buddy_pole_contacts();
-[$response, $field_labels] = scjpc_fetch_jpa_contacts_fields($fields, $jpa_contacts, 'buddy-pole'); ?>
-<div class="excel-table-container excel-table-container-auto-height">
+<div class="excel-table-container">
   <table class="excel-table">
     <thead>
     <tr>
       <?php foreach ($response[array_key_first($response)] as $field => $value) { ?>
-        <th><p><?php echo $field_labels[$field]; ?></p></th>
+        <th><?php echo $field_labels[$field]; ?></th>
       <?php } ?>
     </tr>
     </thead>
@@ -13,7 +11,7 @@
     <?php foreach ($response as $key => $row) { ?>
       <tr>
         <?php foreach ($row as $field => $value) { ?>
-          <td><p><?php echo $value; ?></p></td>
+          <td data-post-id="<?php echo $key; ?>"><?php echo $value; ?></td>
         <?php } ?>
       </tr>
     <?php } ?>
