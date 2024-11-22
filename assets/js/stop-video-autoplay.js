33dbe38ad12e7a337b9b4107a0b3761dfcd749ce
Ali Abbas <ali@accuratewebsolution.com>
2024-11-22 19:37:24 +0500

    stopped the video autoplay

--- assets/js/stop-video-autoplay.js
+++ assets/js/stop-video-autoplay.js
@@ -1,8 +1,25 @@
-jQuery(document).ready(function ($) {
-  $('div.e-tabs-items div').on('click', function (event) {
-    const $contentTabVideo = document.querySelector(`#${$(this).attr('aria-controls')} video`);
-    $contentTabVideo.addEventListener('canplay', function () {
-      $contentTabVideo.pause();
-    })
-  })
-})
+document.addEventListener('DOMContentLoaded', function () {
+  // Select all the div elements inside div.e-tabs-items
+  const tabItems = document.querySelectorAll('div.e-tabs-items div');
+
+  tabItems.forEach(function (tab) {
+    tab.addEventListener('click', function (event) {
+      event.preventDefault();
+
+      // Use setTimeout to delay the execution
+      setTimeout(function () {
+        // Get the value of the aria-controls attribute
+        const ariaControls = tab.getAttribute('aria-controls');
+        const $contentTabVideo = document.querySelector(`#${ariaControls} video`);
+
+        // Check if the video element exists
+        if ($contentTabVideo) {
+          // Add the canplay event listener to the video element
+          $contentTabVideo.addEventListener('canplay', function () {
+            $contentTabVideo.pause();
+          });
+        }
+      }, 500);
+    });
+  });
+});
